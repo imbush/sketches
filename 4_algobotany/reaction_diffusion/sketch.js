9e02f8c964cd193c89474107125457cf9620bb62
imbush <inlebush@gmail.com>
2021-01-10 13:17:33 +0100

    cleaning

--- 4_algobotany/reaction_diffusion/sketch.js
+++ 4_algobotany/reaction_diffusion/sketch.js
@@ -1,8 +1,8 @@
 let pixelHeight, pixelWidth, conc, tempConc;
 let Da = 1.0;
 let Db = 0.5;
-let Dt = 0.01;    // Time Step
-let f = 0.055;
+let Dt = 100;    // Time Step
+let f = 0.0367;
 let k = 0.062;
 
 function setup() {
@@ -17,7 +17,7 @@ function setup() {
         conc[y] = [];
         tempConc[y] = [];
         for (let x = 0; x < pixelWidth + 2; x++) {
-            if (x ** 2 + y ** 2 < (pixelWidth + 2) / 10) {
+            if ((pixelWidth / 2 - x) ** 2 + (pixelHeight / 2 - y) ** 2 < (pixelWidth + 2) / 4) {
                 conc[y][x] = [0, 1];
                 tempConc[y][x] = [0, 1];
             } else {
@@ -30,12 +30,13 @@ function setup() {
 }
 
 function draw() {
+    updateConcentration();
+
     loadPixels();
     for (let x = 1; x < pixelWidth + 1; x++) {
         for (let y = 1; y < pixelHeight + 1; y++) {
-            updateConcentration(x, y)
             pixelIndex = 4 * (x - 1 + (y - 1) * pixelWidth);
-            bright = floor(255 * (tempConc[y][x][0] - tempConc[y][x][1] + 1) / 2);
+            bright = floor(255 * (conc[y][x][0] - conc[y][x][1] + 1) / 2);
             pixels[pixelIndex] = bright;
             pixels[pixelIndex + 1] = bright;
             pixels[pixelIndex + 2] = bright;
@@ -43,37 +44,46 @@ function draw() {
         }
     }
     updatePixels();
-    swap(tempConc, conc);
+    fill(0);
+    circle(mouseX, mouseY, 100);
 }
 
-function updateConcentration(x, y) {
-    let A = conc[y][x][0];
-    let B = conc[y][x][1];
-    console.log(A)
-    let newA = A + (Da * laplacian(x, y, 0) - A * B ** 2 + f * (1 - A)) * Dt;
-    let newB = B + (Db * laplacian(x, y, 1) + A * B ** 2 - (k + f) * B) * Dt;
+function updateConcentration() {
+    let A, B, newA, newB;
+    for (let x = 1; x < pixelWidth + 1; x++) {
+        for (let y = 1; y < pixelHeight + 1; y++) {
+            A = conc[y][x][0];
+            B = conc[y][x][1];
+            // newA = A + (Da * laplacian(x, y, 0) - A * B ** 2 + f * (1 - A)) * Dt;
+            // newB = B + (Db * laplacian(x, y, 1) + A * B ** 2 - (k + f) * B) * Dt;
 
-    tempConc[y][x][0] = newA;
-    tempConc[y][x][1] = newB;
+            // tempConc[y][x][0] = newA;
+            // tempConc[y][x][1] = newB;
+
+            tempConc[y][x][0] = 0.5 * A;
+            tempConc[y][x][1] = 0.5 * B;
+        }
+    }
+    swap(tempConc, conc);
+    console.log('finished')
 }
 
 function laplacian(x, y, index) {
-    let convolution = [[.05, .2, .5],
-                       [.2, -1, .2],
-                       [.05, .2, .5]];
-    let value = 0;
+    let value = - 1 * conc[y][x][index];
+    value += 0.2 * conc[y + 1][x][index];
+    value += 0.2 * conc[y - 1][x][index];
+    value += 0.2 * conc[y][x + 1][index];
+    value += 0.2 * conc[y][x - 1][index];
+    value += 0.05 * conc[y + 1][x + 1][index];
+    value += 0.05 * conc[y + 1][x - 1][index];
+    value += 0.05 * conc[y - 1][x + 1][index];
+    value += 0.05 * conc[y - 1][x - 1][index];
 
-    for (let convX = 0; convX < convolution.length; convX++) {
-        for (let convY = 0; convY < convolution.length; convY++) {
-            value += convolution[convY][convX] * conc[y + convY - 1][x + convX - 1]; 
-        }
-    }
-    
     return value;
 }
 
 function swap(arr1, arr2) {
-    const temp = arr1;
+    let temp = arr1;
     arr1 = arr2;
-    arr2 = arr1;
+    arr2 = temp;
 }
